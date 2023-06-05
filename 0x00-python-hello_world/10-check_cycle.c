4107aaf35a39418335296941e73f8038e78c11d4
gouzidev <gouzi.dev@gmail.com>
2023-06-05 20:34:43 +0100

    commit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,22 @@
 #include "lists.h"
-
+/**
+ * check_cycle - checks cycle
+ * @list: pointer to int node list
+ *
+ * Return: integer indicating if cycle or not
+ */
 int check_cycle(listint_t *list)
 {
-    
-    return (0);
+listint_t *slow = list;
+listint_t *fast = list;
+while (fast)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+{
+return (1);
+}
+}
+return (0);
 }
