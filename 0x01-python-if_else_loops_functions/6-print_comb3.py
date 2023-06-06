57edfce929575d0ffb91543b20696b195071f4c6
gouzidev <gouzi.dev@gmail.com>
2023-06-06 14:41:55 +0100

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(i, 10):
         if i != j:
-            print("{}{}".format(i, j), sep="", end="")
+            print(f"{i}{j}", sep="", end="")
         if j != 9 and i != 9 and j != 0:
             print(", ", sep="", end="")
 print()
