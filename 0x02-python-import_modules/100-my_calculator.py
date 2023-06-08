035a3dcb6a00b142bbf2d6e4c67570d274792710
gouzidev <gouzi.dev@gmail.com>
2023-06-08 13:13:47 +0100

    commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    from sys import argv, exit
     from calculator_1 import add, div, mul, sub
-    if (len(argv) != 3):
+    if (len(argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    a = int(argv(1))
-    op = int(argv(2))
-    b = int(argv(3))
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
     if op == "+":
         res = a + b
         print("{} {} {} = {}".format(a, op, b, res))
@@ -21,3 +22,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, op, b, res))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
