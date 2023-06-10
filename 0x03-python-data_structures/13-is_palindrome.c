49482922bd9af2825f1d9457f6d9d08bd31e2280
gouzidev <gouzi.dev@gmail.com>
2023-06-10 21:34:00 +0100

    commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -29,37 +29,41 @@ return (1);
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *prev = NULL;
-listint_t *curr = *head;
-listint_t *next = *head;
-listint_t *fast = *head;
-int palindrome_comp = 0;
+    listint_t *prev = NULL;
+    listint_t *curr = *head;
+    listint_t *next = *head;
+    listint_t *next_next = *head;
+    int palindrome_comp = 0;
 
-if (head == NULL || *head == NULL)
-return (1);
-if ((*head)->n && (*head)->next == NULL)
-return 1;
+    if (head == NULL || *head == NULL)
+        return (1);
+    if ((*head)->n && (*head)->next == NULL)
+        return 1;
 
-next = curr->next;
-fast = curr->next->next;
-prev = curr;
-curr->next = NULL;
-prev = curr;
-curr = next;
+    next = curr->next;
+    next_next = curr->next->next;
+    prev = curr;
+    curr->next = NULL;
+    prev = curr;
+    curr = next;
 
-while (fast && fast->next)
-{
-fast = fast->next;
-next = curr->next;
-curr->next = prev;
-prev = curr;
-curr = next;
-}
-if (fast)
-next = next->next;
+    while (next_next)
+    {
+        next_next = next_next->next;
+        if (next_next)
+        {
+            next_next = next_next->next;
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        else
+            next = next->next;
+    }
 
-palindrome_comp = comp_linked_lists(prev, next);
+    palindrome_comp = comp_linked_lists(prev, next);
 
-return (palindrome_comp);
+    return (palindrome_comp);
 
 }
