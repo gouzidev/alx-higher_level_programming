cc3c2d47a19640c564153f29c2feb83161d98c87
gouzidev <gouzi.dev@gmail.com>
2023-06-10 22:44:47 +0100

    commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup = (*tuple_a, *tuple_b)
-    return tup
+    tup = []
+    if len(tuple_a) > 1:
+        if len(tuple_b) > 1:
+            tup.append(tuple_a[0] + tuple_b[0])
+            tup.append(tuple_a[1] + tuple_b[1])
+            return tuple(tup)
+        else:
+            if len(tuple_b) == 1:
+                tup.append(tuple_a[0] + tuple_b[0])
+                tup.append(tuple_a[1])
+                return tuple(tup)
+            else:
+                tup.append(tuple_a[0])
+                tup.append(tuple_a[1])
+                return tuple(tup)
+
+    if len(tuple_b) > 1:
+        if len(tuple_a) > 1:
+            tup.append(tuple_b[0] + tuple_a[0])
+            tup.append(tuple_b[1] + tuple_a[1])
+            return tuple(tup)
+        else:
+            if len(tuple_a) == 1:
+                tup.append(tuple_b[0] + tuple_a[0])
+                tup.append(tuple_b[1])
+                return tuple(tup)
+            else:
+                tup.append(tuple_b[0])
+                tup.append(tuple_b[1])
+                return tuple(tup)
