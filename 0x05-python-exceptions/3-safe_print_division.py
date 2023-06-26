898802fa4ce10b22d4c1779075f350328813242f
gouzidev <gouzi.dev@gmail.com>
2023-06-26 23:12:30 +0100

    commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,6 @@
 def safe_print_division(a, b):
-	try:
-		res = a / b
-	except ZeroDivisionError:
-		res = None
-	return res
-	
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    return res
