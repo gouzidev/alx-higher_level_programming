4dd80a5f0355bfe84d012215ca335803855ff96b
gouzidev <gouzi.dev@gmail.com>
2023-06-27 00:59:07 +0100

    commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    res = False
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        sys.stderr.write("Exception: Unknown format code '{}' for object of type 'str'\n".format(value))
+        return False
