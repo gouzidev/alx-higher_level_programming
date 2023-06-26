adc61d34428d0a9ffbc0f5a1c5e883601c163001
gouzidev <gouzi.dev@gmail.com>
2023-06-26 22:23:22 +0100

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='', sep='')
             n_printed += 1
             i += 1
-    except:
+    except Exception:
         return n_printed
     finally:
         print()
