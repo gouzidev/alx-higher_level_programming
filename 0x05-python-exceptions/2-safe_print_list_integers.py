8d3ea2aff6b6a64b5d093a902ea29ef6bb0fbfd1
gouzidev <gouzi.dev@gmail.com>
2023-06-26 23:08:41 +0100

    commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="", sep="")
         except (TypeError, ValueError):
-            pass
-        finally:
-            n += 1
             i += 1
+            pass
+        else:
+            n += 1
     print()
     return n
