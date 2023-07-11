6e7107d51c63752b5a2b504eccb07d37cb48f68c
gouzidev <gouzi.dev@gmail.com>
2023-07-11 21:46:25 +0100

    mine

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,22 +4,15 @@
     The function returns two integers.
     5 line module comment.
 """
-
-
 def add_integer(a, b=98):
-    """ Add two integers and returns the result.
-    Args:
-        a (int): The first integer.
-        b (int): The secodn integer.
-    """
-    if type(a) is not int:
-        if type(a) is float:
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if type(b) is not int:
-        if type(b) is float:
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return (a + b)
+    """ adds two integers 
+    (or floats)
+    and returns sum """
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if type(a) != int or type(b) != int:
+        raise TypeError("a must be an integer or b must be an integer")
+    return a + b
+
