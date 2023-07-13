411f6f77bd38474ba5f4d2afc5dcdcedaeecf798
gouzidev <gouzi.dev@gmail.com>
2023-07-13 20:19:39 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-
 """
-    this module does some calculations
-    maybe
-    5 lines
+    Module containing add_integer function.
+    The function returns two integers.
+    5 line module comment.
 """
 
 
 def add_integer(a, b=98):
-    """ Adds two numbers
+    """ Add two integers and returns the result.
     Args:
-        a : the first
-        b : the second
+        a (int): The first integer.
+        b (int): The secodn integer.
     """
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
     if type(a) is not int:
-        raise TypeError("a must be an integer")
+        if type(a) is float:
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError("b must be an integer")
-    return a + b
+        if type(b) is float:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return (a + b)
