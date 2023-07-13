84dfd5964f5aece709e5ccbcba4f04830e2d46ea
gouzidev <gouzi.dev@gmail.com>
2023-07-13 20:16:37 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,19 +6,18 @@
     5 lines
 """
 
-
 def add_integer(a, b=98):
     """ Adds two numbers
     Args:
         a : the first
         b : the second
     """
-    if type(a) == float:
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
-        b = float(b)
-    if type(a) != int:
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if type(b) is not int:
         raise TypeError("b must be an integer")
     return a + b
