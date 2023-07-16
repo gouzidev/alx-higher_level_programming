2a449c1caf8d9bf06a63fe899760353a4a7aa08a
gouzidev <gouzi.dev@gmail.com>
2023-07-16 16:26:39 +0100

    6-1

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,6 +16,9 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1]), 1)
 
     def test_empty(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
     def test_float(self):
@@ -23,6 +26,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_mixed(self):
         self.assertEqual(max_integer([99, 58, 7, 2.2, 45, 3.3, 1e5]), 1e5)
+
+    def test_tuple(self):
+        self.assertEqual(max_integer((99, 58, 7, 2.2, 45, 3.3, 1e5)), 1e5) 
 
 if __name__ == "__main__":
     unittest.main()
