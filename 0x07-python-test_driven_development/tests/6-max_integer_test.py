9bf29f403d616b7708f5f73d4ba1346a874ab696
gouzidev <gouzi.dev@gmail.com>
2023-07-16 16:30:36 +0100

    6-1

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+"""
+    Unittest for max_integer([..])
+"""
+
+
 class TestMaxInteger(unittest.TestCase):
+    """ class """
     def test_perfect(self):
-        self.assertEqual(max_integer([1,2,3,4]), 4)
+        """ test """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_type(self):
+        """ test """
         with self.assertRaises(TypeError):
-            max_integer([1,2,3,"hi"])
+            max_integer([1, 2, 3, "hi"])
 
     def test_one_value(self):
+        """ test """
         self.assertEqual(max_integer([1]), 1)
 
     def test_empty(self):
+        """ test """
         self.assertEqual(max_integer(), None)
 
     def test_empty_list(self):
+        """ test """
         self.assertEqual(max_integer([]), None)
 
     def test_float(self):
+        """ test """
         self.assertEqual(max_integer([1.1, 2.2, 3.3]), 3.3)
 
     def test_mixed(self):
+        """ test """
         self.assertEqual(max_integer([99, 58, 7, 2.2, 45, 3.3, 1e5]), 1e5)
 
     def test_tuple(self):
-        self.assertEqual(max_integer((99, 58, 7, 2.2, 45, 3.3, 1e5)), 1e5) 
+        """ test """
+        self.assertEqual(max_integer((99, 58, 7, 2.2, 45, 3.3, 1e5)), 1e5)
+
 
 if __name__ == "__main__":
     unittest.main()
